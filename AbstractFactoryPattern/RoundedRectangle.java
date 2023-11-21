42ea170958fe2b8be9b17aca7f7a34832453c92c
Nimsara De Zoysa <105286867+nimdz@users.noreply.github.com>
2023-11-21 12:46:31 +0530

    Create RoundedRectangle.java

---
+++ AbstractFactoryPattern/RoundedRectangle.java
@@ -0,0 +1,8 @@
+package AbstractFactoryPattern;
+
+public class RoundedRectangle implements Shape {
+    
+    public void draw(){
+        System.out.println("Inside RoundedRectangle::draw() method.");
+    }
+}
